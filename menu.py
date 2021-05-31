31f22e7374888bdd1a099dd9558fa00d6e31cee3
lscantelbury <lhsda.snf20@uea.edu.br>
2021-05-30 20:25:50 -0400

    Credits Updates

--- menu.py
+++ menu.py
@@ -1,10 +1,14 @@
 import pygame
 
+pygame.init()
+
 menu_x = 720
 menu_y = 480
 window = pygame.display.set_mode((menu_x, menu_y))
 menu_bg = pygame.image.load('assets/menu/luis.coimbra_menu.png')
 clock = pygame.time.Clock()
+font = pygame.font.Font('assets/PressStart2P.ttf', 10)
+
 
 # defining colors
 black = pygame.Color(0, 0, 0)
@@ -12,6 +16,21 @@ white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(16, 56, 6)  # phtaloh green
 blue = pygame.Color(0, 0, 255)
+
+def credits():
+    menu_bg = pygame.image.load('assets/menu/snake.png')
+    window.blit(menu_bg, (0, 0))
+    pygame.display.set_caption(('Credits'))
+    text = font.render('''Group 6
+    Luís Henrique Coimbra Evangelista
+    Luã Maury Maquiné da Silva
+    Luiz Carlos Glomyer Pereira Gomes Junior
+    Levi Alfaia Loureiro
+    Luís Henrique Scantelbury de Almeida''', True, black)
+    render = text.get_rect()
+    render.midtop = (menu_x / 2, menu_y / 4)
+    window.blit(text, render)
+    pygame.display.update()
 
 def start_menu():
     menu = True
@@ -45,13 +64,14 @@ def start_menu():
             if  290 + 130 >= mouse[0] >= 290 and 325 + 50 >= mouse[1] >= 325:
                 menubg = pygame.image.load('assets/menu/luis.coimbra_menucredits.png')
                 window.blit(menubg, (0, 0))
-                pygame.display.update()
-            
+                pygame.display.flip()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    credits()
             if  290 + 130 >= mouse[0] >= 290 and 395 + 50 >= mouse[1] >= 395:
                 menubg = pygame.image.load('assets/menu/luis.coimbra_menuquit.png')
                 window.blit(menubg, (0, 0))
                 pygame.display.update()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     quit()
-start_menu()        
 
+start_menu()        
